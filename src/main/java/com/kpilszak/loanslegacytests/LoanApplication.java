22e21e7b51577bd0de487ad092e5f8cb833cea46
kpilszak <klaudia.pilszak@gmail.com>
2022-11-21 08:07:20 +0100

    Prepare loan application model

---
+++ src/main/java/com/kpilszak/loanslegacytests/LoanApplication.java
@@ -0,0 +1,67 @@
+package com.kpilszak.loanslegacytests;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class LoanApplication {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int principal; // amount borrowed
+    private int termInMonths;
+    private BigDecimal repayment;
+    private Boolean approved;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(int principal) {
+        this.principal = principal;
+    }
+
+    public int getTermInMonths() {
+        return termInMonths;
+    }
+
+    public void setTermInMonths(int termInMonths) {
+        this.termInMonths = termInMonths;
+    }
+
+    public BigDecimal getRepayment() {
+        return repayment;
+    }
+
+    public void setRepayment(BigDecimal repayment) {
+        this.repayment = repayment;
+    }
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public void setApproved(Boolean approved) {
+        this.approved = approved;
+    }
+}
