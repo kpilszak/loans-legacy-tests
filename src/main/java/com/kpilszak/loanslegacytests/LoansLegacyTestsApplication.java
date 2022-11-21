30bfd2aae0223726515f353ef2580fa54f24962d
kpilszak <klaudia.pilszak@gmail.com>
2022-11-21 07:52:30 +0100

    Init repo

---
+++ src/main/java/com/kpilszak/loanslegacytests/LoansLegacyTestsApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.loanslegacytests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansLegacyTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansLegacyTestsApplication.class, args);
+    }
+
+}
