4e56c515237e305f346d42846778f8d04d8f65eb
kpilszak <klaudia.pilszak@gmail.com>
2022-11-22 11:45:55 +0100

    Test repayment with spying object

--- src/test/java/com/kpilszak/loanslegacytests/RepaymentAmountTest.java
+++ src/test/java/com/kpilszak/loanslegacytests/RepaymentAmountTest.java
@@ -1,12 +1,31 @@
 package com.kpilszak.loanslegacytests;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito.*;
+import org.mockito.Spy;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 public class RepaymentAmountTest {
 
+    @Spy
+    LoanApplication loanApplication;
+
     @Test
-    public void test1YearLoanWholePounds() {}
+    public void test1YearLoanWholePounds() {
+        loanApplication = spy (new LoanApplication());
+        loanApplication.setPrincipal(1200);
+        loanApplication.setTermInMonths(12);
+        doReturn(new BigDecimal(10)).when(loanApplication).getInterestRate();
+
+        LoanApplicationController controller = new LoanApplicationController();
+        controller.processNewLoanApplicationForm(loanApplication);
+
+        assertEquals(new BigDecimal(110), loanApplication.getRepayment());
+    }
 
     @Test
     public void test2YearLoanWholePounds() {}
