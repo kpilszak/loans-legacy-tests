ecb92fce62e136c78ec442c39b026f2b385da93d
kpilszak <klaudia.pilszak@gmail.com>
2022-11-22 11:28:56 +0100

    Add Mockito test framework

---
+++ src/test/java/com/kpilszak/loanslegacytests/RepaymentAmountTest.java
@@ -0,0 +1,15 @@
+package com.kpilszak.loanslegacytests;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class RepaymentAmountTest {
+
+    @Test
+    public void test() {
+        fail();
+    }
+
+}
